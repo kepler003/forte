be6384e266a0f28003cc2e6dfb9cebda3441a512
Bartek <b.pyzocha@gmail.com>
2022-05-03 14:04:47 +0200

    Load intern from db

--- src/EditIntern.jsx
+++ src/EditIntern.jsx
@@ -1,28 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { NavLink } from 'react-router-dom';
 
 const EditIntern = () => {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [intern, setIntern] = useState({});
 
-    useEffect(() => {
-        //TODO: get intern from REST api http://localhost:3001/interns/:id
-        console.log(`I want to get intern with id: ${id}!`)
-    }, [id]);
+  // Load intern from db
+  useEffect(() => {
+    fetch(`http://localhost:3001/interns/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((intern) => setIntern(intern))
+      .catch((err) => {
+        console.log('Error while fetching intern data');
+        throw err;
+      });
+  }, [id]);
 
-    return (
-        <div>
-            <NavLink to="/">Back to list </NavLink>
-            <form>
-                <label>Name</label>
-                <input type="text" name="name" />              
-                <label>Email</label>
-                <input type="text" name="email" />
-                
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <NavLink to='/'>Back to list </NavLink>
+      <form>
+        <label>Name</label>
+        <input type='text' name='name' />
+        <label>Email</label>
+        <input type='text' name='email' />
+
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
+  );
 };
 
 export default EditIntern;
--- src/index.jsx
+++ src/index.jsx
@@ -8,9 +8,7 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <Router>
+    <App />
+  </Router>
 );
