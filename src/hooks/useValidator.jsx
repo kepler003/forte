be040aa2bdacdfe39a191afcb39b1e623680674a
Bartek <b.pyzocha@gmail.com>
2022-05-03 16:55:05 +0200

    Add email & date validation

--- src/hooks/useValidator.jsx
+++ src/hooks/useValidator.jsx
@@ -19,6 +19,24 @@ const useValidator = (value, config = {}) => {
     validate(value, 'This field is required');
   }, [value, empty]);
 
+  // Check if email is incorrect
+  useEffect(() => {
+    if (value === undefined) return;
+    if (type !== 'email') return;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    validate(regex.test(value), 'This email address is incorrect');
+  }, [value, type]);
+
+  // Check if date is after min date
+  useEffect(() => {
+    if (value === undefined) return;
+    if (type !== 'date') return;
+    if (!min) return;
+    const currDate = Date.parse(value.replace('+00Z', ':00.000Z'));
+    const minDate = Date.parse(min.replace('+00Z', ':00.000Z'));
+    validate(minDate < currDate, 'This date is incorrect');
+  }, [value, type, min]);
+
   return errors;
 };
 
