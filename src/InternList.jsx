2e71f0f501199191f50f95ae2104b32220aa7890
Bartek <b.pyzocha@gmail.com>
2022-05-04 18:51:50 +0200

    Clean up

--- src/InternList.jsx
+++ src/InternList.jsx
@@ -2,23 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const InternList = () => {
+  const [interns, setInterns] = useState([]);
 
-    const [interns, setInterns] = useState([]);
+  useEffect(() => {
+    const fetchInterns = async () => {
+      const response = await fetch('http://localhost:3001/interns');
+      const interns = await response.json();
+      setInterns(interns);
+    };
+    fetchInterns();
+  }, []);
 
-    useEffect(() => {
-        const fetchInterns = async () => {
-            const response = await fetch('http://localhost:3001/interns');
-            const interns = await response.json();
-            setInterns(interns);
-        }
-        fetchInterns();
-    }, []);
-
-    return (
-        <div>
-            {interns.map(u => (<div key={u.id}>{u.name} <NavLink to={`/interns/${u.id}`}>Edit</NavLink></div>))}
+  return (
+    <div>
+      {interns.map((u) => (
+        <div key={u.id}>
+          {u.name} <NavLink to={`/interns/${u.id}`}>Edit</NavLink>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default InternList;
