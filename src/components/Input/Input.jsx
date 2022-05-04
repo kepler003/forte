4f9e79e8468b87cd08894104a9aeb5f34d564d4b
Bartek <b.pyzocha@gmail.com>
2022-05-04 21:34:28 +0200

    Add date input variant

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import Icon from '../Icon/Icon';
 import cls from './Input.module.css';
 
-const Input = ({ label, error, className, ...props }) => {
+const Input = ({ label, type, error, className, ...props }) => {
   const inputRef = useRef();
 
   useEffect(() => {
@@ -13,21 +13,48 @@ const Input = ({ label, error, className, ...props }) => {
     }
   }, [error]);
 
+  const dateIconClickHandler = () => {
+    inputRef.current.focus();
+    inputRef.current.showPicker();
+  };
+
   const labelClassNames = [cls.label, 'text-medium-l'].join(' ');
   const inputClassNames = [className, cls.input, 'text-l'].join(' ');
 
   const labelElem = label && <label className={labelClassNames}>{label}</label>;
-  const iconElem = error && (
-    <Icon className={cls.icon} icon='alert' color='#a3270c' />
+
+  const warningIconElem = error && (
+    <Icon
+      className={type === 'date' ? cls.dateWarningIcon : cls.warningIcon}
+      icon='alert'
+      color='#a3270c'
+    />
   );
+
+  const dateIconElem = type === 'date' && (
+    <Icon
+      className={cls.dateIcon}
+      icon='calendar'
+      onClick={dateIconClickHandler}
+    />
+  );
+
   const errorElem = error && <p className={cls.error}>{error}</p>;
+
+  console.log(cls);
 
   return (
     <div className={cls.box}>
       {labelElem}
       <div className={cls.inputBox}>
-        <input ref={inputRef} className={inputClassNames} {...props} />
-        {iconElem}
+        <input
+          type={type}
+          className={inputClassNames}
+          ref={inputRef}
+          {...props}
+        />
+        {warningIconElem}
+        {dateIconElem}
       </div>
       {errorElem}
     </div>
