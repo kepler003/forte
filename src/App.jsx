29c60bac2a6c4bb75677d1ee90f59079a6b81fa3
Bartek <b.pyzocha@gmail.com>
2022-05-04 23:38:19 +0200

    Remove Main component

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import EditIntern from './components/EditIntern/EditIntern';
 import InternList from './components/InternList/InternList';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Main>
+      <main className='main'>
         <Routes>
           <Route path='/interns/:id' exact element={<EditIntern />} />
           <Route path='/' element={<InternList />} />
         </Routes>
-      </Main>
+      </main>
     </div>
   );
 }
--- src/components/Main/Main.jsx
+++
@@ -1,7 +0,0 @@
-import cls from './Main.module.css';
-
-const Main = ({ className, ...props }) => {
-  return <main className={[className, cls.main].join(' ')} {...props} />;
-};
-
-export default Main;
