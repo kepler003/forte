9c96f9f610c0fbf8b2c3c47b8d7b94147171b40c
Bartek <b.pyzocha@gmail.com>
2022-05-04 23:06:10 +0200

    Make logo a NavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import cls from './Header.module.css';
 import logo from '../../images/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className={cls.header}>
-      <img src={logo} alt='logo' />
+      <NavLink to='/'>
+        <img src={logo} alt='logo' />
+      </NavLink>
     </header>
   );
 };
